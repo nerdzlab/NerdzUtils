7e5f226d46bb78189ab08b600ef4e85ee942c533
Roman Kovalchuk <roman.kovalchuk@nerdzlab.com>
2020-09-28 20:39:49 +0300

    Added keychain property, changed defaults property wrapper

---
+++ Sources/NerdzUtils/Extensions/Formatter+iso8601.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  
+//
+//  Created by Roman Kovalchuk on 28.09.2020.
+//
+
+import Foundation
+
+public extension Formatter {
+    
+    ///
+    /// Return iso8601 formatter
+    ///
+    static let iso8601noFS = ISO8601DateFormatter()
+    
+    static let iso8601: ISO8601DateFormatter = {
+        let formatter = ISO8601DateFormatter()
+        formatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
+        return formatter
+    }()
+}
