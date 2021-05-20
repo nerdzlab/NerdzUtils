c22ef5daa764dc3c64be8cca1e497186f1964b7c
Vasyl Khmil <vasyl@nerdzlab.com>
2021-05-20 15:45:18 +0300

    - added publicity;

--- Sources/NerdzUtils/Extensions/UI/UIImage+Scale.swift
+++ Sources/NerdzUtils/Extensions/UI/UIImage+Scale.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-extension UIImage {
+public extension UIImage {
     func scaled(toWidth value: CGFloat) -> UIImage {
         let scale = value / size.width
         return scaled(by: scale)
