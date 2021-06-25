090abaa8a6063648aaf4c61668d16f2dee4d1228
Vasyl Khmil <vasyl@nerdzlab.com>
2021-06-25 09:49:53 +0300

    - added supported platform;

--- Package.swift
+++ Package.swift
@@ -5,16 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "NerdzUtils",
+    platforms: [
+            .iOS(.v12)
+    ],
+    
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "NerdzUtils",
             targets: ["NerdzUtils"]),
     ],
+    
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", from: "4.2.0")
     ],
+    
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import NerdzUtilsTests
+
+var tests = [XCTestCaseEntry]()
+tests += NerdzUtilsTests.allTests()
+XCTMain(tests)
