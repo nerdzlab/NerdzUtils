286cb6e81dd47bf4f56577f6db31a364014f6bce
Vasyl Khmil <vasylkhmil1995@gmail.com>
2022-05-19 18:38:32 +0300

    - added locale transferring;

--- Sources/NerdzUtils/Extensions/String+Localization.swift
+++ Sources/NerdzUtils/Extensions/String+Localization.swift
@@ -7,9 +7,27 @@
 
 import Foundation
 
+private let overridenLacaleKey = "nz.overridenLocale"
+
 public extension String {
     /// Return localized representation of current string
     var localized: String {
-        NSLocalizedString(self, comment: "")
+        var bundle: Bundle = .main
+        
+        if let path = Bundle.main.path(forResource: String.overridenLocale, ofType: "lproj") {
+            bundle = Bundle(path: path) ?? .main
+        }
+        
+        return NSLocalizedString(self, tableName: nil, bundle: bundle, value: "", comment: "")
+    }
+    
+    static var overridenLocale: String? {
+        set {
+            UserDefaults.standard.set(newValue, forKey: overridenLacaleKey)
+        }
+        
+        get {
+            UserDefaults.standard.string(forKey: overridenLacaleKey)
+        }
     }
 }
